7b46e0026e48d22c614300ec0307bf6dfb4b3172
Ophir LOJKINE <pere.jobs@gmail.com>
2016-12-06 22:48:37 +0300

    Retry tile load when tiles are corrupted
    And start loading from tile 0x0
    
    Fixes #2

--- dezoom.sh
+++ dezoom.sh
@@ -4,37 +4,47 @@ if [ $# -ne 3 ]
 then
   echo "$0: invalid number of arguments
 
-  Usage: $0 width height URL
-    width: width of the full image in number of tiles
-    height: height of the full image in number of tiles
+  Usage: $0 max_x max_y URL
+    max_x: width of the full image in number of tiles
+    max_y: height of the full image in number of tiles
     URL: the URL of individual tiles, with the x position replaced by %X and the y position replaced by %Y";
   exit 1;
 fi
 
-width_in_tiles=$1
-height_in_tiles=$2
+max_x=$1
+max_y=$2
 url_template=$3
+width_in_tiles=$(( $max_x + 1 ))
+height_in_tiles=$(( $max_y + 1 ))
 
 export TMPDIR=$(mktemp -d)
 
 function download_file {
   outfile=$1
   url=$2
+  retried=$3
+
   wget --timeout 10 -O "$outfile" "$url" 2>/dev/null
-  if [[ $(file "$outfile") != *"image"* ]]
+  tilesize=$(identify -format "%wx%h" "$outfile")
+  if [ $tilesize ]
   then
+    echo $tilesize > "$TMPDIR/tilesize.txt"
+  else
     rm -f "$outfile"
     echo "Failed to download '$url'"
-  else
-    identify -format "%wx%h\n" "$outfile" >> "$TMPDIR/tilesizes.txt"
+    if [ ! $retried ]
+    then
+      sleep 1
+      download_file $outfile $url true
+    fi
   fi
 }
 
 filelist=""
-for y in $(seq $height_in_tiles)
+for y in $(seq 0 $max_y)
 do
   echo -ne "Downloading... $((100*$y/$height_in_tiles))%    \r"
-  for x in $(seq $width_in_tiles)
+  for x in $(seq 0 $max_x)
   do
     url=$(echo $url_template | sed "s/%X/$x/" | sed "s/%Y/$y/")
 
@@ -47,7 +57,7 @@ done
 
 echo -e "\nTiles added."
 
-tilesize=$(head -n 1 "$TMPDIR/tilesizes.txt")
+tilesize=$(< "$TMPDIR/tilesize.txt")
 i=0
 for f in $filelist
 do
